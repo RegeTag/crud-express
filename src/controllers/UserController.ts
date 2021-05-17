3edba01408ea96fc74e3d8d1697016c1dfbe865a
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2021-05-17 18:18:31 -0300

    added update on controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -60,6 +60,34 @@ class UserController{
 
         return res.status(200).json(user)
     }
+
+    async update(req:Request, res:Response){
+        const userRepository = getCustomRepository(UserRepository)
+
+        const {id} = req.params
+
+        const {firstName, lastName, password} = req.body
+
+        const user = await userRepository.findOne({where:{id}})
+
+        if(!user){
+            return res.status(400).json({"message":"couldn't find user!"})
+        }  
+
+        if(user.password != password){
+            return res.status(400).json({"message": "Current password wrong!"})
+        }
+
+        const updatedUser = userRepository.create({
+            ...user,
+            first_name: !firstName ? user.first_name : firstName,
+            last_name: !lastName ? user.last_name : lastName
+        })
+
+        await userRepository.save(updatedUser)
+
+        return res.status(200).json({"message": "User updated with success!"})
+    }
 }
 
 export default new UserController()
--- src/router.ts
+++ src/router.ts
@@ -6,5 +6,6 @@ const router = Router()
 router.get("/users", UserController.getAll)
 router.get("/users/:id", UserController.getOne)
 router.post("/users", UserController.create)
+router.put("/users/:id", UserController.update)
 
 export default router
